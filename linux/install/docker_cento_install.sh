6c542f1053196feb24672e3bfc12aaaa085cb525
fortunexfortune <osadolor.fortune@yahoo.it>
2019-04-05 17:48:22 +0100

    modified docker install file

--- linux/install/docker_cento_install.sh
+++ linux/install/docker_cento_install.sh
@@ -2,6 +2,8 @@ sudo yum -y install docker
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo groupadd docker
-sudo usermod -aG docker jenkins
-sudo systemctl restart jenkins
+sudo usermod -aG docker $USER
 sudo systemctl restart docker
+
+#sudo usermod -aG docker jenkins
+#sudo systemctl restart jenkins
