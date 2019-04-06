3c26aba0b38417458e682ac157f9b440687bbd40
fortunexfortune <osadolor.fortune@yahoo.it>
2019-04-06 09:35:50 +0100

    updated visual code install script

--- linux/install/visual_code._install.sh
+++ linux/install/visual_code._install.sh
@@ -2,7 +2,7 @@
 wget https://github.com/codercom/code-server/releases/download/1.604-vsc1.32.0/code-server1.604-vsc1.32.0-linux-x64.tar.gz
 tar xvfz code-server1.604-vsc1.32.0-linux-x64.tar.gz
 cd code-server1.604-vsc1.32.0-linux-x64
-mv code-server /bin
+sudo mv code-server /bin
 cd ..
 rm -rf code-server1.604-vsc1.32.0-linux-x64
 rm code-server1.604-vsc1.32.0-linux-x64.tar.gz
