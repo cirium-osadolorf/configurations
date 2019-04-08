07d9345bbc0ec2274093b708f36f24dea98e4f25
fortunexfortune <osadolor.fortune@yahoo.it>
2019-04-08 13:41:34 +0100

    added terraform install file for centos

---
+++ linux/install/terraform.sh
@@ -0,0 +1,8 @@
+#! /bin/nash
+
+curl https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_linux_amd64.zip
+--output terraform_0.11.8_linux_amd64.zip
+unzip terraform_0.11.8_linux_amd64.zip
+mkdir /bin/terraform 
+mv terraform_0.11.8_linux_amd64 /bin/terraform
+rm -fR terraform_0.11.8_linux_amd64.zip
