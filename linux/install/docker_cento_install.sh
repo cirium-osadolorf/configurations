2eac843d339f72a3e6c39f3117c8066ca64020be
fortunexfortune <osadolor.fortune@yahoo.it>
2019-04-08 14:22:45 +0100

    updated docker intall file

--- linux/install/docker_cento_install.sh
+++ linux/install/docker_cento_install.sh
@@ -1,9 +1,32 @@
-sudo yum -y install docker
+#! /bin/bash
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install docker-ce docker-ce-cli containerd.io
+
+
+
+# sudo yum -y install docker
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo systemctl restart docker
 
+sudo docker run hello-world
 #sudo usermod -aG docker jenkins
 #sudo systemctl restart jenkins
