76e60faec97b29b9719c23471a1155e84afde2ed
fortunexfortune <osadolor.fortune@yahoo.it>
2019-04-09 15:17:26 +0100

    added type = map

--- devops/terraform/5-tfvars/variable.tf
+++ devops/terraform/5-tfvars/variable.tf
@@ -9,11 +9,17 @@ variable "image" {
 }
 variable "container_name" {
   description = "Name of container"
+  type ="map"
+
 
 }
 variable "int_port" {
   description = "internal port for container"
+  type ="map"
+
 }
 variable "ext_port" {
   description = "external port for container"
+  type ="map"
+
 }
