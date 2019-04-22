cc5537255f2f19996971413be0f48d2d1262c137
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-04-22 09:30:25 +0100

    Update docker_cento_install.sh

--- linux/install/docker_cento_install.sh
+++ linux/install/docker_cento_install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # Link for the  full instructions https://docs.docker.com/install/linux/docker-ce/centos/
-yum remove -y docker \
+sudo yum remove -y docker \
                   docker-client \
                   docker-client-latest \
                   docker-common \
@@ -9,23 +9,22 @@ yum remove -y docker \
                   docker-logrotate \
                   docker-engine
 
-yum install -y yum-utils \
+sudo yum install -y yum-utils \
   device-mapper-persistent-data \
   lvm2
 
 
-yum-config-manager \
+sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
 
-yum install -y docker-ce docker-ce-cli containerd.io
+sudo yum install -y docker-ce docker-ce-cli containerd.io
 
-systemctl start docker
-systemctl enable docker
-groupadd docker
-usermod -aG docker $USER
-usermod -aG docker $LOGNAME
-systemctl restart docker
-systemctl status docker
-docker run hello-world
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo systemctl restart docker
+sudo systemctl status docker
+sudo docker run hello-world
 #sudo chmod 766 configurations/linux/install/docker_cento_install.sh &&  sudo configurations/linux/install/docker_cento_install.sh
