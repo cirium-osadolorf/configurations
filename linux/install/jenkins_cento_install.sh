3910b916ebefb7430cf14a7347acc2510f51ac7c
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-04-22 19:05:04 +0100

    Update jenkins_cento_install.sh

--- linux/install/jenkins_cento_install.sh
+++ linux/install/jenkins_cento_install.sh
@@ -8,5 +8,5 @@ yum -y install jenkins
 systemctl start jenkins
 systemctl status jenkins
 systemctl enable jenkins
-cat /var/jenkins_home/secrets/initialAdminPassword
+cat /var/lib/jenkins/secrets/initialAdminPassword
 #sudo chmod 766 configurations/linux/install/jenkins_cento_install.sh &&  sudo configurations/linux/install/jenkins_cento_install.sh
