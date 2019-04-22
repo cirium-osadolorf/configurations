89e10bff883301ec122b019c9b5871d7f61c8736
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-04-22 10:24:47 +0100

    Update kubernetes_cento_install.sh

--- linux/install/kubernetes_cento_install.sh
+++ linux/install/kubernetes_cento_install.sh
@@ -16,4 +16,7 @@ yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
     
-yum install docker-ce-docker-ce-18.06.1 docker-ce-cli-18.06.1 containerd.io
+yum install docker-ce-docker-ce-18.06.1.ce-3.el7
+yum update --exclude=docker
+
+#sudo chmod 766 configurations/linux/install/kubernetes_cento_install.sh &&  sudo configurations/linux/install/kubernetes_cento_install.sh
