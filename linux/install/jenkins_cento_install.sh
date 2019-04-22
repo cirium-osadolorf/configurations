efedad91f1915c475792e53edc54d728bc707967
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-04-22 19:00:59 +0100

    Update jenkins_cento_install.sh

--- linux/install/jenkins_cento_install.sh
+++ linux/install/jenkins_cento_install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # script explanation is in the folloeing link :
 # https://linuxize.com/post/how-to-install-jenkins-on-centos-7/
-yum install java-1.8.0-openjdk-devel
+yum install -y java-1.8.0-openjdk-devel
 curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
 rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 yum -y install jenkins
