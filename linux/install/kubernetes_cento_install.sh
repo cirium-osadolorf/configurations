6df9c68fc3a4cf0f7c3f0adcf0c24608fd56f0f8
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-04-22 15:52:13 +0100

    Update kubernetes_cento_install.sh

--- linux/install/kubernetes_cento_install.sh
+++ linux/install/kubernetes_cento_install.sh
@@ -44,10 +44,11 @@ EOF
 # Set SELinux in permissive mode (effectively disabling it)
 setenforce 0
 sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
 yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
-systemctl enable --now kubelet
 yum versionlock add kubelet kubeadm kubectl
 yum list updates | cat -n
+systemctl enable --now kubelet
 kubeadm version
 
 #sudo chmod 766 configurations/linux/install/kubernetes_cento_install.sh &&  sudo configurations/linux/install/kubernetes_cento_install.sh
