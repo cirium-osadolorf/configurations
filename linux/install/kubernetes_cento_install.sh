af015e19d22514c5cefe5250c3e70e1f3fa92bf9
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-04-22 10:19:12 +0100

    Update kubernetes_cento_install.sh

--- linux/install/kubernetes_cento_install.sh
+++ linux/install/kubernetes_cento_install.sh
@@ -1,1 +1,19 @@
-
+yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+                  
+                  
+yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2                  
+                  
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+    
+yum install docker-ce-docker-ce-18.06.1 docker-ce-cli-18.06.1 containerd.io
