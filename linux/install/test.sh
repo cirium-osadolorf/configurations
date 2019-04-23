93bdeab020a29cb5f6281ed724fd99dbeb876802
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-04-23 16:57:15 +0100

    Create test.sh

---
+++ linux/install/test.sh
@@ -0,0 +1,64 @@
+
+#! /bin/bash
+#Kubernetes setup with kubeadm, kubectl adn kubelet
+
+#Remove docker dependencies 
+yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+                  
+                  
+yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2                  
+                  
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+    
+#installing docker 
+yum install -y docker-ce-18.06.1.ce-3.el7 
+
+#Prevents docker from ever updating
+yum -y install yum-versionlock
+yum versionlock add docker-ce
+yum versionlock list
+yum list updates | cat -n
+
+
+#Starting docker
+systemctl start docker
+systemctl enable docker
+systemctl status docker
+docker run hello-world
+
+#Kubernetes setup
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kube*
+EOF
+
+# Set SELinux in permissive mode (effectively disabling it)
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+#instakk kubeadm , kubectl and kubelet
+yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+systemctl enable --now kubelet
+kubeadm version
+
+cd    
+kubeadm init --pod-network-cidr=10.244.0.0/16 >> kubeadm_init  
+
+#sudo chmod 766 configurations/linux/install/test.sh &&  sudo configurations/linux/install/test.sh
