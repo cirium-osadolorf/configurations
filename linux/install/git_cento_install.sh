1a088943a865cdb1e68f31e93e970bda5120e61c
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-04-23 14:30:54 +0100

    Update git_cento_install.sh

--- linux/install/git_cento_install.sh
+++ linux/install/git_cento_install.sh
@@ -1,1 +1,1 @@
-yum install -y git  && git config --global user.email "osadolor.fortune@yahoo.it" && git config --global user.name "fortunexfortune"
+sudo yum install -y git  && git config --global user.email "osadolor.fortune@yahoo.it" && git config --global user.name "fortunexfortune"
