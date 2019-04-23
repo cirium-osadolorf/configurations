7e7fd3708b2a9e646c8ae754c19d166bf57c6da5
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-04-23 18:38:18 +0100

    Update kubernetes_cento_master_install.sh

--- linux/install/kubernetes_cento_master_install.sh
+++ linux/install/kubernetes_cento_master_install.sh
@@ -59,13 +59,9 @@ kubeadm version
 
 cd    
 kubeadm init --pod-network-cidr=10.244.0.0/16 >> kubeadm_init    
-# mkdir -p $HOME/.kube
-# cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-# chown $(id -u):$(id -g) $HOME/.kube/config
-
-cp /etc/kubernetes/admin.conf $HOME/
-chown $(id -u):$(id -g) $HOME/admin.conf
-export KUBECONFIG=$HOME/admin.conf
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl version
 echo "============================================"
