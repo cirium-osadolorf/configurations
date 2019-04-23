967cf2a3fd4d27a1905d61ee27ac542f2a94b152
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-04-23 19:40:35 +0100

    Update docker_cento_install.sh

--- linux/install/docker_cento_install.sh
+++ linux/install/docker_cento_install.sh
@@ -27,4 +27,4 @@ usermod -aG docker $USER
 systemctl restart docker
 systemctl status docker
 docker run hello-world
-#cd ~ && sudo chmod 766 configurations/linux/install/docker_cento_install.sh && mv configurations/linux/install/docker_cento_install.sh /usr/bin && docker_cento_install.sh
+#cd ~ && sudo chmod 766 configurations/linux/install/docker_cento_install.sh && cp configurations/linux/install/docker_cento_install.sh bin && sudo docker_cento_install.sh
