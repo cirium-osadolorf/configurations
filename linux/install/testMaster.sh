c42ebab0b4d97dee6678998ad43094e95d95fa74
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-04-24 13:17:16 +0100

    Update testMaster.sh

--- linux/install/testMaster.sh
+++ linux/install/testMaster.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 #Kubernetes setup with kubeadm, kubectl adn kubelet
 
-HOMEPATH=$1
-
 #Remove docker dependencies 
 yum remove docker \
                   docker-client \
@@ -59,30 +57,5 @@ yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 systemctl enable --now kubelet
 kubeadm version
 
-cd    
-kubeadm init --pod-network-cidr=10.244.0.0/16 >> kubeadm_init    
-mkdir -p $HOMEPATH/.kube
-cp -i /etc/kubernetes/admin.conf $HOMEPATH/.kube/config
-chown $(id -u):$(id -g) $HOMEPATH/.kube/config
-
-kubectl version
-echo "============================================"
-tail -n 6 kubeadm_init
-echo "============================================"
-
-echo "have you joined your workder nodes to the cluster? (yes / no) "
-read INPUT
-kubectl get nodes
-
-#Flannel setup
-# echo "net.bridge.bridge-nf-call-iptables=1" | tee -a /etc/sysctl.conf
-# sysctl -p
-# kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
-# echo "============================================"
-# echo"Verifying that all the nodes now have a STATUS of Ready:"
-# kubectl get nodes
-# echo "============================================"
-# echo"Verifying that all the fannel pods are Ready:"
-# kubectl get pods -n kube-system 
 
 #sudo chmod 766 configurations/linux/install/testMaster.sh &&  sudo configurations/linux/install/testMaster.sh $HOME
