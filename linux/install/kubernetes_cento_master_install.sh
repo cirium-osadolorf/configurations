f660e05bcffddfed6083d3736e566380f4f89c59
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-04-24 11:03:34 +0100

    Update kubernetes_cento_master_install.sh

--- linux/install/kubernetes_cento_master_install.sh
+++ linux/install/kubernetes_cento_master_install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #Kubernetes setup with kubeadm, kubectl adn kubelet
-USERHOME=$1
+#USERHOME=$1
 
 #Remove docker dependencies 
 yum remove docker \
@@ -60,9 +60,9 @@ kubeadm version
 
 cd    
 kubeadm init --pod-network-cidr=10.244.0.0/16 >> kubeadm_init    
-mkdir -p $USERHOME/.kube
-cp -i /etc/kubernetes/admin.conf $USERHOME/.kube/config
-chown $(id -u):$(id -g) $USERHOME/.kube/config
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl version
 echo "============================================"
