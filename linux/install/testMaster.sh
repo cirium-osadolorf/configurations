8b2bf297f054b0d4467b4c626e4b1f7ee93517d3
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-04-24 11:47:45 +0100

    Update testMaster.sh

--- linux/install/testMaster.sh
+++ linux/install/testMaster.sh
@@ -75,14 +75,14 @@ read INPUT
 kubectl get nodes
 
 #Flannel setup
-echo "net.bridge.bridge-nf-call-iptables=1" | tee -a /etc/sysctl.conf
-sysctl -p
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
-echo "============================================"
-echo"Verifying that all the nodes now have a STATUS of Ready:"
-kubectl get nodes
-echo "============================================"
-echo"Verifying that all the fannel pods are Ready:"
-kubectl get pods -n kube-system 
+# echo "net.bridge.bridge-nf-call-iptables=1" | tee -a /etc/sysctl.conf
+# sysctl -p
+# kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
+# echo "============================================"
+# echo"Verifying that all the nodes now have a STATUS of Ready:"
+# kubectl get nodes
+# echo "============================================"
+# echo"Verifying that all the fannel pods are Ready:"
+# kubectl get pods -n kube-system 
 
 #sudo chmod 766 configurations/linux/install/testMaster.sh &&  sudo configurations/linux/install/testMaster.sh $HOME
