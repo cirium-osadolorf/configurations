7d7c9193593ac07d371ecc16ad046a006639e9ea
fortunexfortune <osadolor.fortune@yahoo.it>
2019-04-30 15:16:44 +0100

    updated kubernetes  cento install 2

--- linux/install/Kubernates_cento_Install2/kubernetes_cento_master_install.sh
+++ linux/install/Kubernates_cento_Install2/kubernetes_cento_master_install.sh
@@ -58,27 +58,34 @@ systemctl enable --now kubelet
 kubeadm version
 
 #Bootstrap the cluster
-kubeadm init --pod-network-cidr=10.244.0.0/16  
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl version
-echo "============================================"
-echo "have you joined your workder nodes to the cluster? (yes / no) "
-read INPUT
-kubectl get nodes
+kubeadm init --pod-network-cidr=10.244.0.0/16  | tail -n 2  > hash_token.txt
+while read -r LINE; do 
+  ONELINER="$ONELINER $LINE"
+done < "hash_token.txt"  
+echo "${ONELINER///}" > output
+cat output
 
 
-#Flannel setup (Networking)
-echo "net.bridge.bridge-nf-call-iptables=1" | tee -a /etc/sysctl.conf
-sysctl -p
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
-echo "============================================"
-echo"Verifying that all the nodes now have a STATUS of Ready:"
-kubectl get nodes
-echo "============================================"
-echo"Verifying that all the fannel pods are Ready:"
-kubectl get pods -n kube-system 
+# mkdir -p $HOME/.kube
+# cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+# chown $(id -u):$(id -g) $HOME/.kube/config
+# kubectl version
+# echo "============================================"
+# echo "have you joined your workder nodes to the cluster? (yes / no) "
+# read INPUT
+# kubectl get nodes
+
+
+# #Flannel setup (Networking)
+# echo "net.bridge.bridge-nf-call-iptables=1" | tee -a /etc/sysctl.conf
+# sysctl -p
+# kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
+# echo "============================================"
+# echo"Verifying that all the nodes now have a STATUS of Ready:"
+# kubectl get nodes
+# echo "============================================"
+# echo"Verifying that all the fannel pods are Ready:"
+# kubectl get pods -n kube-system 
 #kubectl taint nodes --all node-role.kubernetes.io/master-  # allows Kubernetes / terraform to place pods on the master node
 
 #sudo chmod 766 configurations/linux/install/kubernetes_cento_master_install.sh &&  sudo configurations/linux/install/kubernetes_cento_master_install.sh && mkdir -p $HOME/.kube && sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && sudo chown $(id -u):$(id -g) $HOME/.kube/config
