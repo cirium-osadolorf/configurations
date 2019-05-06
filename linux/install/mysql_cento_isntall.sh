ba9e82eca7b023720d8bcde9a1d863a99f8e0219
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-05-06 21:55:20 +0100

    Update mysql_cento_isntall.sh

--- linux/install/mysql_cento_isntall.sh
+++ linux/install/mysql_cento_isntall.sh
@@ -1,11 +1,1 @@
-sudo yum localinstall https://dev.mysql.com/get/mysql80-community-release-el7-1.noarch.rpm
-sudo yum install mysql-community-server
-sudo yum localinstall https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
-sudo yum update
-sudo yum install mysql-community-server
-sudo systemctl start mysqld
-sudo systemctl enable mysqld
-sudo systemctl status sysmysqld
 
-# For security Configuration check the following resources:
-# https://linuxize.com/post/install-mysql-on-centos-7/
