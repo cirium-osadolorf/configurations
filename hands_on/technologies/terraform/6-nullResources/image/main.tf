6789af8d32c34ff46609f33e76ff9bd1a54323f1
fortunexfortune <osadolor.fortune@yahoo.it>
2019-05-08 11:54:12 +0000

    moved files to private repo

--- hands_on/technologies/terraform/6-nullResources/image/main.tf
+++
@@ -1,4 +0,0 @@
-# Download the latest Ghost Image
-resource "docker_image" "image_id" {
-  name = "${var.image}"
-}
--- hands_on/technologies/terraform/6-nullResources/image/output.tf
+++
@@ -1,5 +0,0 @@
-
-#Output the IP Address of the Container
-output "image_out" {
-  value = "${docker_image.image_id.latest}"
-}
