eabce3bd5107af352d9aea0530a035a54da84f64
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-05-10 19:00:56 +0100

    Update dockercompose_cento_install.sh

--- linux/install/dockercompose_cento_install.sh
+++ linux/install/dockercompose_cento_install.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
-
+#sudo chmod 766 configurations/linux/install/dockercompose_cento_install.sh && sudo configurations/linux/install/dockercompose_cento_install.sh
