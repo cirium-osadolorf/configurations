c73ccf5c3c53ab9eee31374d8a8614d90c6146ec
fortunexfortune <osadolor.fortune@yahoo.it>
2019-05-10 16:34:39 +0000

    added docker compose file

---
+++ linux/install/visual_code._install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+wget https://github.com/codercom/code-server/releases/download/1.604-vsc1.32.0/code-server1.604-vsc1.32.0-linux-x64.tar.gz
+tar xvfz code-server1.604-vsc1.32.0-linux-x64.tar.gz
+cd code-server1.604-vsc1.32.0-linux-x64
+sudo mv code-server /bin
+cd ..
+rm -rf code-server1.604-vsc1.32.0-linux-x64
+rm code-server1.604-vsc1.32.0-linux-x64.tar.gz
+
+#mkdir myapp
+#code-server 
