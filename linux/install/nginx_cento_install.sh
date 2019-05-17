b0b0bc24abb642117b4fc5f4918f9c37a22e90fd
fortunexfortune <osadolor.fortune@yahoo.it>
2019-05-17 09:31:25 +0000

    nginx updated

--- linux/install/nginx_cento_install.sh
+++ linux/install/nginx_cento_install.sh
@@ -4,5 +4,3 @@ yum install  -y epel-release
 yum install -y  nginx
 systemctl start nginx
 systemctl enable nginx
-
-#sudo chmod 766 configurations/linux/install/nginx_cento_install.sh && sudo configurations/linux/install/nginx_cento_install.sh
