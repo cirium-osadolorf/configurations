05a68ccfa0cd7bb16d533ef02bc23eaacb21cc36
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-05-28 08:25:50 +0100

    Update jenkins_install.sh

--- linux/install/ubuntu/jenkins_install.sh
+++ linux/install/ubuntu/jenkins_install.sh
@@ -9,7 +9,7 @@ else
 fi
 
 #Jenkins Install
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | apt-key add -
 sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 apt update -y
 apt install -y jenkins
