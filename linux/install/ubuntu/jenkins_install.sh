78f42312d477c0b55c69384cd1a42dd2aeb8a359
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-05-28 07:26:07 +0100

    Create jenkins_install.sh

---
+++ linux/install/ubuntu/jenkins_install.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+  
+if java -version  2>> /dev/null; then
+        echo "Java is installed on this machine"
+else
+        echo "installing Java"
+        apt update -y
+        apt install -y openjdk-8-jdk
+fi
+
+#Jenkins Install
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt update -y
+apt install -y jenkins
+systemctl start jenkins
+systemctl status jenkins
+systemctl enable jenkins
+
+
+echo "=================================================="
+echo "Use the below command to obtain you Jenkins Password : "
+echo "sudo cat /var/lib/jenkins/secrets/initialAdminPassword"
+
+
+
+#ufw enable
+#ufw default deny
+#ufw status 
