d1862a220cb179e26c441abae856bd6d94fa7432
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-05-29 10:00:03 +0100

    Update nginx_isntall.sh

--- linux/install/ubuntu/nginx_isntall.sh
+++ linux/install/ubuntu/nginx_isntall.sh
@@ -2,7 +2,6 @@
 
 apt-get update -y
 apt-get install -y nginx
-systemctl start nginx
 systemctl enable nginx
-#sudo chmod 766 configurations/linux/install/ubuntu/nginx_isntall.sh && sudo configurations/linux/install/ubuntu/nginx_isntall.sh
 
+#sudo chmod 766 configurations/linux/install/ubuntu/nginx_isntall.sh && sudo configurations/linux/install/ubuntu/nginx_isntall.sh
