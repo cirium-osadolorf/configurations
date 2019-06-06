dfe9b42a339f117a8fa0b41a795d4216389efefd
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-06-06 12:59:46 +0100

    Update git_install.sh

--- linux/install/centos/git_install.sh
+++ linux/install/centos/git_install.sh
@@ -7,4 +7,4 @@ sudo yum install -y git  && git config --global user.email "osadolor.fortune@yah
 sudo yum install -y git  && git config --global user.email "osadolor.fortune@yahoo.it" && git config --global user.name "fortunexfortune" && git clone https://github.com/FortunexFortune/configurations.git
 
 
-git config --system core.editor vim && git config --global user.email "osadolor.fortune@yahoo.it" && git config --global user.name "fortunexfortune"
+git config --global core.editor vim && git config --global user.email "osadolor.fortune@yahoo.it" && git config --global user.name "fortunexfortune"
