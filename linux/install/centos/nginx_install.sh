16fad22b2bbc3fff443eec844e4ed635104b092b
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-06-09 23:09:36 +0100

    Update nginx_install.sh

--- linux/install/centos/nginx_install.sh
+++ linux/install/centos/nginx_install.sh
@@ -4,7 +4,7 @@ touch /etc/yum.repos.d/nginx.repo
 cat << EOF > /etc/yum.repos.d/nginx.repo
 [nginx]
 name=nginx repo
-baseurl=http://nginx.org/packages/centos/$7/$basearch/
+baseurl=http://nginx.org/packages/centos/7/$basearch/
 gpgcheck=0
 enabled=1
 EOF
