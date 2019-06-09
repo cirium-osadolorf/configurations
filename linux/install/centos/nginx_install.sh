a70493fd9869598f23096d087e6880231b1b9d9b
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-06-09 22:00:13 +0100

    Update nginx_install.sh

--- linux/install/centos/nginx_install.sh
+++ linux/install/centos/nginx_install.sh
@@ -1,23 +1,18 @@
 #! /bin/bash
 
-yum install  -y epel-release
-yum install -y  nginx
+touch /etc/yum.repos.d/nginx.repo
+cat << EOF > /etc/yum.repos.d/nginx.repo
+[nginx]
+name=nginx repo
+baseurl=http://nginx.org/packages/centos/7/$basearch/
+gpgcheck=0
+enabled=1
+EOF
+
+yum update -y
+yum install -y nginx
 systemctl start nginx
 systemctl enable nginx
-
-# alternative method of installing nginx 
-# touch /etc/yum.repos.d/nginx.repo
-# cat << EOF > /etc/yum.repos.d/nginx.repo
-#[nginx]
-#name=nginx repo
-#baseurl=http://nginx.org/packages/centos/7/$basearch/
-#gpgcheck=0
-#enabled=1
-# EOF
-# yum update -y
-# yum install -y nginx
-# systemctl start nginx
-# systemctl enable nginx
-# systemctl status nginx
+systemctl status nginx
 
 #sudo chmod 766 configurations/linux/install/centos/nginx_install.sh && sudo configurations/linux/install/centos/nginx_install.sh
