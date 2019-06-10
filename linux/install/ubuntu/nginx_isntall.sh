a01a2d9fd20ad333515b361c8e2942f3f9dabc89
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-06-10 09:39:36 +0100

    Update nginx_isntall.sh

--- linux/install/ubuntu/nginx_isntall.sh
+++ linux/install/ubuntu/nginx_isntall.sh
@@ -1,7 +1,18 @@
 #!  /bin/bash
 
+curl -o nginx_signing.key http://nginx.org/keys/nginx_signing.key
+apt-key add nginx_signing.key
+#rm -rf nginx_signing.key
+
+echo "## Add official NGINX repository" >> /etc/apt/sources.list
+echo "deb http://nginx.org/packages/ubuntu/ xenial nginx" >> /etc/apt/sources.list
+echo "deb-src http://nginx.org/packages/ubuntu/ xenial nginx" >>  /etc/apt/sources.list
+
 apt-get update -y
 apt-get install -y nginx
+
+systemctl start nginx
 systemctl enable nginx
+systemctl status nginx
 
 #sudo chmod 766 configurations/linux/install/ubuntu/nginx_isntall.sh && sudo configurations/linux/install/ubuntu/nginx_isntall.sh
