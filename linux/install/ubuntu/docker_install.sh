dbcc537343b08817db29b33dcb18dcfce469a120
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-06-11 09:35:51 +0100

    Update docker_install.sh

--- linux/install/ubuntu/docker_install.sh
+++ linux/install/ubuntu/docker_install.sh
@@ -14,7 +14,7 @@ apt-get -y install \
   software-properties-common
 
 #Add the Docker GPG key and repo
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
