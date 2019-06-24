3fd4adc2fe51a0859d1bb96eb1f76818d4ae9895
Fortune Osadolor <33355125+FortunexFortune@users.noreply.github.com>
2019-06-24 16:49:24 +0100

    Update dockercompose_install.sh

--- linux/install/centos/dockercompose_install.sh
+++ linux/install/centos/dockercompose_install.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
-
 curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
-
-
 #sudo chmod 766 configurations/linux/install/centos/dockercompose_install.sh && sudo configurations/linux/install/centos/dockercompose_install.sh
