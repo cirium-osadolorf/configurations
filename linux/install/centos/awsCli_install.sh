1d5531a324f8d75d1b1e908eb9621f40ffa421dc
Fortune Osadolor <33355125+FortunexFortune@users.noreply.github.com>
2019-06-27 10:36:01 +0100

    Update awsCli_install.sh

--- linux/install/centos/awsCli_install.sh
+++ linux/install/centos/awsCli_install.sh
@@ -1,7 +1,13 @@
 #! /bin/bash 
+yum -y install python-pip
+pip install --upgrade pip
+pip install awscli
+pip install awscli --upgrade
 
-
-
-
-
+echo "example configuration"
+echo" AWS Access Key ID [****************M5YB]:"
+echo "AWS Secret Access Key [****************I5C1]:"
+echo "Default region name [eu-central-1]:"
+echo "Default output format [json]:"
+aws configure
 #
