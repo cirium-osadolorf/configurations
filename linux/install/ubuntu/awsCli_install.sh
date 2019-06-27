b1d1c12e87b14f83566b0931d9e9bd75ea7f489c
Fortune Osadolor <33355125+FortunexFortune@users.noreply.github.com>
2019-06-27 12:57:43 +0100

    Update awsCli_install.sh

--- linux/install/ubuntu/awsCli_install.sh
+++ linux/install/ubuntu/awsCli_install.sh
@@ -1,18 +1,11 @@
-#! /bin/bash 
+#! /bin/bash
 
-apt-get install -y  software-properties-common
-apt-add-repository universe
-apt-get update -y
-apt-get install -y  python-pip
-pip install --upgrade pip
-pip install awscli
-pip install awscli --upgrade
-
+apt install awscli
+aws --version
 echo "example configuration"
-echo" AWS Access Key ID [****************M5YB]:"
-echo "AWS Secret Access Key [****************I5C1]:"
-echo "Default region name [eu-central-1]:"
-echo "Default output format [json]:"
+echo" AWS Access Key ID : [****************M5YB]"
+echo "AWS Secret Access Key : [****************I5C1]"
+echo "Default region name  : [eu-central-1]"
+echo "Default output format : [json or text]"  
 aws configure
 aws --version
-#
