dd562c997abba8b1ba2d49eb0bb215fbd673cc79
Fortune Osadolor <33355125+FortunexFortune@users.noreply.github.com>
2019-06-27 23:07:49 +0100

    Update awsCli_install.sh

--- linux/install/ubuntu/awsCli_install.sh
+++ linux/install/ubuntu/awsCli_install.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
-apt install awscli
-aws --version
+: ${1?"USAGE: $1 USER" } 
+
+apt-get update
+apt-get -y install python-pip
+pip install --upgrade pip
+pip install awscli
+pip install awscli --upgrade
 echo " ============================================== "
 echo "example configuration"
 echo" AWS Access Key ID : [****************M5YB]"
@@ -9,33 +14,11 @@ echo "AWS Secret Access Key : [****************I5C1]"
 echo "Default region name  : [eu-central-1]"
 echo "Default output format : [json or text]"  
 echo " ============================================== "
-aws configure
-aws --version
-
-
-
-
-
-
-
-#! /bin/bash
-apt-get install -y software-properties-common
-apt-add-repository universe
-apt-get update -y
-apt-get install -y python-pip
-apt-get install -y python-dev python-pip
-pip install awscli
-
-echo " ============================================== "
-echo "example configuration"
-echo "AWS Access Key ID : [****************M5YB]"
-echo "AWS Secret Access Key : [****************I5C1]"
-echo "Default region name  : [eu-central-1]"
-echo "Default output format : [json or text]"  
-echo " ============================================== "
-
-aws configure
+aws configure --profile $USER
 aws --version
 
 echo "To set default user use: "
 echo "export AWS_DEFAULT_PROFILE=USER "
+
+
+#https://computingforgeeks.com/how-to-install-and-use-aws-cli-on-linux-ubuntu-debian-centos/
