80007ced8ea1b935dc03e8744d2d4e0b0d57ecb8
Fortune Osadolor <33355125+FortunexFortune@users.noreply.github.com>
2019-07-04 20:15:40 +0100

    Update awsCli_install.sh

--- linux/install/ubuntu/awsCli_install.sh
+++ linux/install/ubuntu/awsCli_install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-: ${1?"USAGE: $1 AWS_USER "}
+#: ${1?"USAGE: $1 AWS_USER "}
 
 sudo apt-get update
 sudo apt-get -y install python-pip
@@ -14,15 +14,11 @@ echo "AWS Secret Access Key : [****************I5C1]"
 echo "Default region name  : [eu-central-1]"
 echo "Default output format : [json or text]"  
 echo " ============================================== "
-aws configure --profile $AWS_USER
+aws configure
+#aws configure --profile $AWS_USER
 aws --version
-
 sudo apt-get install -y python3-pip python3 python3-setuptools
 pip3 install boto3 --user
-
 echo "To set default user use: "
 echo "export AWS_DEFAULT_PROFILE=USER "
-
-
-
 #https://computingforgeeks.com/how-to-install-and-use-aws-cli-on-linux-ubuntu-debian-centos/
