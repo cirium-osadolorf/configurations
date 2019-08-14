836d0fb3fce31e1731b98230dc7df453eebfdd2a
Fortune Osadolor <33355125+FortunexFortune@users.noreply.github.com>
2019-08-14 19:34:37 +0100

    Update essentials_install.sh

--- linux/install/ubuntu/essentials_install.sh
+++ linux/install/ubuntu/essentials_install.sh
@@ -8,6 +8,8 @@ apt install openjdk-8-jdk
 apt-get install -y tree
 apt-get install -y dialog
 apt-get install -y dos2unix  
+apt-get install -y cmatrix
+
 
 #sudo chmod 766 configurations/linux/install/ubuntu/essentials_install.sh && sudo configurations/linux/install/ubuntu/essentials_install.sh
 
