03f45cea0d52fa4d4a515f3ee7666a6300cd6a8f
Fortune Osadolor <33355125+FortunexFortune@users.noreply.github.com>
2020-08-07 23:21:19 +0100

    Update terraform_install.sh

--- linux/install/ubuntu/terraform_install.sh
+++ linux/install/ubuntu/terraform_install.sh
@@ -1,10 +1,12 @@
 ! /bin/bash
 
-curl https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_linux_amd64.zip --output terraform_0.11.8_linux_amd64.zip
+export VER="0.12.29"
+curl https://releases.hashicorp.com/terraform/${VER}/terraform_${VER}_linux_amd64.zip --output terraform_${VER}_linux_amd64.zip
 apt-get install -y unzip
-unzip terraform_0.11.8_linux_amd64.zip
+unzip terraform_${VER}_linux_amd64.zip
 mkdir /bin/terraform
 mv terraform /usr/local/bin/
-rm -fR terraform_0.11.8_linux_amd64.zip
+rm -fR terraform_${VER}_linux_amd64.zip
 terraform -version
+
 #sudo chmod 766 configurations/linux/install/ubuntu/terraform_install.sh && sudo configurations/linux/install/ubuntu/terraform_install.sh
