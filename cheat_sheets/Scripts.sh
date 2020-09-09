1603c529d902088292a81fab8cebb1159237a3a7
Fortune Osadolor <33355125+FortunexFortune@users.noreply.github.com>
2020-09-09 21:39:31 +0100

    Update Scripts.sh

--- cheat_sheets/Scripts.sh
+++ cheat_sheets/Scripts.sh
@@ -79,6 +79,16 @@ TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 
 
 
+#####################################
+
+for instance in master-1 master-2; do
+  scp ca.crt ca.key kube-apiserver.key kube-apiserver.crt \
+    service-account.key service-account.crt \
+    etcd-server.key etcd-server.crt \
+    ${instance}:~/
+done
+
+
 
 
 
